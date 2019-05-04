a497adb35764b39a9dcbe467e34dc13745449ff8
고태광 <kotaekwang@gotaegwang-ui-MacBook-Pro.local>
2019-05-04 16:04:47 +0900

    init kuzal B/E project

---
+++ src/main/java/com/kuzal/kuzalcompetition/KuzalcompetitionApplication.java
@@ -0,0 +1,13 @@
+package com.kuzal.kuzalcompetition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuzalcompetitionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KuzalcompetitionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kuzal/kuzalcompetition/KuzalcompetitionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kuzal.kuzalcompetition;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KuzalcompetitionApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}

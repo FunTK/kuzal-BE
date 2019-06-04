3e3b623bf70410916cf4769c9f9562da0c3dd6f9
고태광 <kotaekwang@gotaegwang-ui-MacBook-Pro.local>
2019-06-05 00:20:26 +0900

    add pid setting code

--- src/main/java/com/kuzal/kuzalcompetition/KuzalcompetitionApplication.java
+++ src/main/java/com/kuzal/kuzalcompetition/KuzalcompetitionApplication.java
@@ -2,12 +2,15 @@ package com.kuzal.kuzalcompetition;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class KuzalcompetitionApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(KuzalcompetitionApplication.class, args);
+        SpringApplication application = new SpringApplication(KuzalcompetitionApplication.class);
+        application.addListeners(new ApplicationPidFileWriter());
+        application.run(args);
     }
 
 }

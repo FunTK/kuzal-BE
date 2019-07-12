ebe54376dea397833083362047dd1d247ea125c4
박보영 <groot@xhift.co.kr>
2019-07-12 09:52:41 +0900

    feat(Video): get Video by Video_id

--- src/main/java/com/kuzal/kuzalcompetition/controller/VideoController.java
+++ src/main/java/com/kuzal/kuzalcompetition/controller/VideoController.java
@@ -4,11 +4,10 @@ package com.kuzal.kuzalcompetition.controller;
 import com.kuzal.kuzalcompetition.model.Video;
 import com.kuzal.kuzalcompetition.service.VideoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/video")
@@ -20,6 +19,11 @@ public class VideoController {
     @RequestMapping(method = RequestMethod.GET)
     List<Video> getVideos(){
         return videoService.getVideoList();
+    }
+
+    @GetMapping("/{id}")
+    Optional<Video> getVideo(@PathVariable("id") String id) {
+        return videoService.getVideo(id);
     }
 
 }
--- src/main/java/com/kuzal/kuzalcompetition/service/VideoService.java
+++ src/main/java/com/kuzal/kuzalcompetition/service/VideoService.java
@@ -4,8 +4,11 @@ import com.kuzal.kuzalcompetition.model.Video;
 
 
 import java.util.List;
+import java.util.Optional;
 
 
 public interface VideoService {
      List<Video> getVideoList();
+
+    Optional<Video> getVideo(String id);
 }

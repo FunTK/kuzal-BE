18c6eeeda32bba9dddc2a1a15b7dd08d0e5d16e4
taekwang <rhxorhkd@gmail.com>
2019-07-21 10:27:10 +0900

    remvoe querydslPre...

--- src/main/java/com/kuzal/kuzalcompetition/repository/VideoRepository.java
+++ src/main/java/com/kuzal/kuzalcompetition/repository/VideoRepository.java
@@ -2,10 +2,10 @@ package com.kuzal.kuzalcompetition.repository;
 
 import com.kuzal.kuzalcompetition.model.Video;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface VideoRepository extends MongoRepository<Video,String>, QuerydslPredicateExecutor<Video>, VideoCustomRepository {
+public interface VideoRepository extends MongoRepository<Video,String>,
+        VideoCustomRepository {
 }

932cdd32390fd8c44442f1c29ae52bb273ef972a
taekwang <rhxorhkd@gmail.com>
2019-09-05 22:11:46 +0900

    add update user api and get detail user api

--- src/main/java/com/kuzal/kuzalcompetition/repository/UserCustomRepository.java
+++ src/main/java/com/kuzal/kuzalcompetition/repository/UserCustomRepository.java
@@ -1,7 +1,9 @@
 package com.kuzal.kuzalcompetition.repository;
 
 import com.kuzal.kuzalcompetition.model.User;
+import com.mongodb.client.result.UpdateResult;
 
 public interface UserCustomRepository {
     User getUserByEmailAddress(String emailAddress);
+    UpdateResult updateUserByEmailAddress(String emailAddress, String nickName);
 }
---
+++ src/main/java/com/kuzal/kuzalcompetition/request/UserUpdateRequest.java
@@ -0,0 +1,8 @@
+package com.kuzal.kuzalcompetition.request;
+
+import lombok.Data;
+
+@Data
+public class UserUpdateRequest {
+    private String nickName;
+}
